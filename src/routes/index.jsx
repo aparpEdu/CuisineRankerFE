1c7e93460cc26f4ab55d9a63d4b453eeea9d411e
moldovexc <alexanderparpulansky@gmail.com>
2024-01-19 21:02:27 +0200

    Login form created

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,5 +1,6 @@
 import Layout from "../components/layout/Layout";
 import {createBrowserRouter} from "react-router-dom";
+import Login from "../pages/Login";
 // import Home from "../pages/Home";
 // import Projects from "../pages/Projects";
 // import Archive from "../pages/Archive";
@@ -12,8 +13,8 @@ export const router = createBrowserRouter([
         element: <Layout />,
         children: [
             {
-                // index: true,
-                // element: <Projects/>
+                index: true,
+                element: <Login showHeader={false} />
             }
         ],
         // children: [
