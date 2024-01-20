81cc6472ffbb4b7b2cf7a66bb45306afe074236b
moldovexc <alexanderparpulansky@gmail.com>
2024-01-20 22:00:32 +0200

    removed unnecessary comments

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -2,10 +2,6 @@ import Layout from "../components/layout/Layout";
 import {createBrowserRouter} from "react-router-dom";
 import Login from "../pages/Login";
 import Profile from "../pages/Profile";
-// import Home from "../pages/Home";
-// import Projects from "../pages/Projects";
-// import Archive from "../pages/Archive";
-
 
 export const router = createBrowserRouter([
     {
@@ -22,18 +18,5 @@ export const router = createBrowserRouter([
                 path: "/profile"
             }
         ],
-        // children: [
-        //     {
-        //         index: true,
-        //         element: <Home />,
-        //         loader: getAllSpecialties,
-        //     },
-        //     {
-        //         path: "/specialists",
-        //         element: <Specialists />,
-        //         loader: getSpecialistData,
-        //         action: getSpecialistsSettings,
-        //     },
-        // ],
     },
 ]);
