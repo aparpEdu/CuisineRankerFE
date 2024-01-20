1c91d775bac2b5079fbf8cf904fc7ca04938f245
moldovexc <alexanderparpulansky@gmail.com>
2024-01-20 16:22:41 +0200

    Retrieving user data from backend

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,15 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./Profile.css";
-import {Link} from "react-router-dom";
-import GearIcon from "../assets/cogwheel.svg"
+import { Link } from "react-router-dom";
+import GearIcon from "../assets/cogwheel.svg";
 
 const Profile = () => {
     const [level, setLevel] = useState(5);
     const [experience, setExperience] = useState(70);
+    const [userData, setUserData] = useState(() => {
+        const storedUserData = localStorage.getItem("user_data");
+        return storedUserData ? JSON.parse(storedUserData) : {};
+    });
 
     const handleSignOut = () => {
         console.log("Signing out...");
     };
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const token = localStorage.getItem("access_token");
+                const headers = {
+                    Authorization: `Bearer ${token}`,
+                };
+                const response = await axios.get(
+                    "http://localhost:443/api/v1/users",
+                    {
+                        headers,
+                    }
+                );
+                const { name, email } = response.data;
+                const newUserData = { name, email };
+
+
+                setUserData(newUserData);
+
+                localStorage.setItem("user_data", JSON.stringify(newUserData));
+            } catch (error) {
+                console.error("Error fetching user data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     return (
         <div className="profile-container">
@@ -17,13 +50,12 @@ const Profile = () => {
                 <div className="profile-picture-container">
                     <div className="profile-picture">
                         <div className="level-circle">{level}</div>
-                        {}
                     </div>
                 </div>
                 <div className="profile-info-container">
                     <div className="profile-info">
-                        <div className="name">First Name Last Name</div>
-                        <div className="email">email@example.com</div>
+                        <div className="name">{userData.name}</div>
+                        <div className="email">{userData.email}</div>
                     </div>
                 </div>
                 <div className="experience-bar-container">
