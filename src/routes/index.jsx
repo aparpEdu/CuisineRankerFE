edda7aad527564a4a44f04f406a15d4b5ea3eadb
moldovexc <alexanderparpulansky@gmail.com>
2024-01-20 15:03:28 +0200

    added profile page path

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,6 +1,7 @@
 import Layout from "../components/layout/Layout";
 import {createBrowserRouter} from "react-router-dom";
 import Login from "../pages/Login";
+import Profile from "../pages/Profile";
 // import Home from "../pages/Home";
 // import Projects from "../pages/Projects";
 // import Archive from "../pages/Archive";
@@ -15,6 +16,10 @@ export const router = createBrowserRouter([
             {
                 index: true,
                 element: <Login showHeader={false} />
+            },
+            {
+                element: <Profile/>,
+                path: "/profile"
             }
         ],
         // children: [
