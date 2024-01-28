3bde22dbe5b9944e2e560832834c192619cf2918
moldovexc <alexanderparpulansky@gmail.com>
2024-01-28 18:51:35 +0200

    Added email confirmed layout and style

---
+++ src/components/email_confirm/EmailConfirm.jsx
@@ -0,0 +1,32 @@
+import "./EmailConfirm.css"
+import React, { useState } from "react";
+import CodeConfirm from "../code_confirm/CodeConfirm";
+const EmailConfirm = () => {
+    const [showCodeConfirmation, setShowCodeConfirmation] = useState(false);
+    const handleShowCodeConfirmation = () => {
+        setShowCodeConfirmation(true);
+    };
+
+    return (
+        <div className="success-container">
+            {showCodeConfirmation ? (
+                <CodeConfirm />
+            ) : (
+                <>
+                    <h2>Successful Registration!</h2>
+                    <p>
+                        Thank you for signing up! An email has been sent for confirmation.
+                        If you haven't received an email, please check your spam folder.
+                    </p>
+                    <p>
+                        If you encounter any issues or didn't receive the email, you can manually
+                        confirm your account by entering the confirmation code{" "}
+                        <button onClick={handleShowCodeConfirmation}>here</button>.
+                    </p>
+                </>
+            )}
+        </div>
+    );
+};
+
+export default EmailConfirm;
