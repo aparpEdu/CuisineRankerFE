4db1969a0969dd41a378034d2ffb6f9fc56326bf
moldovexc <alexanderparpulansky@gmail.com>
2024-02-29 01:41:17 +0200

    Added redirect to my recipes to dashboard

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './Dashboard.css';
 import cookies from '../../assets/cookies.png'
+import {Link} from "react-router-dom";
 const Dashboard = () => {
     return (
         <div className="dashboard">
+            <Link to={"/recipes"}>
             <div className="action-item">
+
                 <img src={cookies} alt="Action 1" />
                 <span className="action-text">My Recipes</span>
             </div>
+            </Link>
             <div className="action-item">
                 <img src={cookies} alt="Action 2" />
                 <span className="action-text">W.I.P</span>
@@ -20,38 +24,8 @@ const Dashboard = () => {
                 <img src={cookies} alt="Action 3" />
                 <span className="action-text">W.I.P</span>
             </div>
-            <div className="action-item">
-                <img src={cookies} alt="Action 3" />
-                <span className="action-text">W.I.P</span>
-            </div>
-            <div className="action-item">
-                <img src={cookies} alt="Action 3" />
-                <span className="action-text">W.I.P</span>
-            </div>
-            <div className="action-item">
-                <img src={cookies} alt="Action 3" />
-                <span className="action-text">W.I.P</span>
-            </div>
-            <div className="action-item">
-                <img src={cookies} alt="Action 3" />
-                <span className="action-text">W.I.P</span>
-            </div>
-            <div className="action-item">
-                <img src={cookies} alt="Action 3" />
-                <span className="action-text">W.I.P</span>
-            </div>
-            <div className="action-item">
-                <img src={cookies} alt="Action 3" />
-                <span className="action-text">W.I.P</span>
-            </div>
-            <div className="action-item">
-                <img src={cookies} alt="Action 3" />
-                <span className="action-text">W.I.P</span>
-            </div>
-            <div className="action-item">
-                <img src={cookies} alt="Action 3" />
-                <span className="action-text">W.I.P</span>
-            </div>
+
+
 
 
 
