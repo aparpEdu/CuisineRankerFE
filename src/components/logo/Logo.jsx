516107cb581ca99b12a48ba03000354da9b7b8f1
moldovexc <alexanderparpulansky@gmail.com>
2024-03-04 18:27:32 +0200

    Made it possible to redirect to homepage when clicking on logo

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -3,10 +3,10 @@ import logo from "../../assets/logo.svg"
 
 const Logo = () => {
     return (
-    <div className="logo-container">
-        <img src={logo} alt="Logo" className="logo-img"/>
-    </div>
+        <div className="logo-container">
+            <img src={logo} alt="Logo" className="logo-img" />
+        </div>
     );
-}
+};
 
 export default Logo;
