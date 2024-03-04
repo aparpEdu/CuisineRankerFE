c0215f42f6e85d5b4018420b844ab36efdc58c08
moldovexc <alexanderparpulansky@gmail.com>
2024-03-04 18:28:35 +0200

    Added a settings nav bar for traversing settings options

---
+++ src/components/settings_nav_bar/SettingsNavBar.jsx
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+import "../nav_bar/NavigationBar.css"
+import Star from "../../assets/star.svg"
+import {Link} from "react-router-dom";
+
+const SettingsNavBar = () => {
+    const [currentPage, setCurrentPage] = useState("General");
+
+
+    const handlePageClick = (page) => {
+        setCurrentPage(page);
+    };
+
+    return (
+        <>
+            <nav className={`sidebar-settings`}>
+
+                <ul className="nav-items-settings">
+                    <li className={`nav-item ${currentPage === "General" ? "active" : ""}`}>
+                        <Link to={"/settings"}  onClick={() => handlePageClick("General")}>General</Link>
+                        {currentPage === "General" && <span className="arrow"><img src={Star} alt={"star"}/> </span>}
+                    </li>
+                    <li className={`nav-item ${currentPage === "Password" ? "active" : ""}`}>
+                        <Link  to={"/explore"} onClick={() => handlePageClick("Password")}>Password</Link>
+                        {currentPage === "Password" && <span className="arrow"><img src={Star} alt={"star"}/></span>}
+                    </li>
+                    <li className={`nav-item ${currentPage === "Email" ? "active" : ""}`}>
+                        <Link  to={"/explore"} onClick={() => handlePageClick("Email")}>Email</Link>
+                        {currentPage === "Email" && <span className="arrow"><img src={Star} alt={"star"}/></span>}
+                    </li>
+                </ul>
+            </nav>
+        </>
+    );
+};
+
+
+export default SettingsNavBar;
