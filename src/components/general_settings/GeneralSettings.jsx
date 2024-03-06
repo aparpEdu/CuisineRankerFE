35654f37bccd636ac02c9038753da84797d74f0b
moldovexc <alexanderparpulansky@gmail.com>
2024-03-06 20:41:26 +0200

    Refactored settings to work with different components instead of urls

---
+++ src/components/general_settings/GeneralSettings.jsx
@@ -0,0 +1,136 @@
+import React, {useEffect, useState} from "react";
+import api from "../../services/api";
+
+const GeneralSettings = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
+    const [initialFirstName, setInitialFirstName] = useState("");
+    const [initialLastName, setInitialLastName] = useState("");
+    const [errors, setErrors] = useState({});
+    const [error, setError] = useState("");
+    const [isDisabled, setDisabled] = useState(true)
+    const [success, setSuccess] = useState("");
+
+    useEffect(() => {
+        const fetchUserData = async () => {
+            try {
+                const response = await api.get('/users');
+                const data = response.data;
+                const name = data.name.split(" ");
+                setFirstName(name.at(0))
+                setLastName(name.at(1))
+                setInitialFirstName(name[0]);
+                setInitialLastName(name[1]);
+            } catch (error) {
+                console.error("Error fetching user data:", error);
+            }
+        }
+        fetchUserData()
+    }, []);
+
+    const handleInputChange = (e) => {
+        setSuccess("");
+        const { id, value } = e.target;
+        if (
+            (id === "firstName" && value !== initialFirstName)  ||
+            (id === "lastName" && value !== initialLastName)
+        ) {
+            setDisabled(false);
+        } else {
+            setDisabled(true);
+        }
+
+        if (id === "firstName") {
+            setFirstName(value);
+            delete errors.firstName;
+        } else if (id === "lastName") {
+            setLastName(value);
+            delete errors.lastName;
+        }
+
+        if (value.trim() === "" || (id === "firstName" && lastName.trim() === "") || (id === "lastName" && firstName.trim() === "")) {
+            setDisabled(true);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        const validationErrors = validateInput();
+
+        if (Object.keys(validationErrors).length > 0) {
+            setErrors(validationErrors);
+            setDisabled(true);
+            return;
+        }
+        try {
+            const response = await api.patch("/users", {
+                firstName: firstName,
+                lastName: lastName
+            });
+            setInitialFirstName(response.data.firstName);
+            setInitialLastName(response.data.lastName);
+            setDisabled(true);
+            setSuccess("Saved changes");
+            setErrors([])
+        } catch (e) {
+            setError(e.response.data.message);
+        }
+    };
+
+    const validateInput = () => {
+
+        if (!firstName.trim()) {
+            errors.firstName = "Please enter a first name.";
+            setDisabled(true);
+        } else if (!/^[A-Za-z'-. ]{1,50}$/.test(firstName)) {
+            errors.firstName = "Please enter a valid first name.";
+            setDisabled(true);
+        } else {
+            delete errors.firstName;
+            setErrors(errors);
+        }
+
+        if (!lastName.trim()) {
+            errors.lastName = "Please enter a last name.";
+            setDisabled(true);
+        } else if (!/^[A-Za-z'-. ]{1,50}$/.test(lastName)) {
+            errors.lastName = "Please enter a valid last name.";
+            setDisabled(true);
+        }
+        return errors;
+    };
+
+    return (
+      <div>
+          <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                  <label htmlFor="firstName">First Name:</label>
+                  <input
+                      type="text"
+                      id="firstName"
+                      value={firstName}
+                      onChange={handleInputChange}
+                  />
+
+              </div>
+              {errors.firstName && <span className="error">{errors.firstName}</span>}
+              <div className="form-group">
+                  <label htmlFor="lastName">Last Name:</label>
+                  <input
+                      type="text"
+                      id="lastName"
+                      value={lastName}
+                      onChange={handleInputChange}
+                  />
+              </div>
+              {errors.lastName && <span className="error">{errors.lastName}</span>}
+              {error && <p style={{ color: "red" }}>{error}</p>}
+              {success && <p style={{ color: "lime" }} className="error-success">{success}</p>}
+              <button type="submit" disabled={isDisabled} className="button-save">SAVE CHANGES</button>
+
+          </form>
+      </div>
+    );
+};
+
+export default GeneralSettings;
--- src/components/settings_nav_bar/SettingsNavBar.jsx
+++ src/components/settings_nav_bar/SettingsNavBar.jsx
@@ -3,36 +3,57 @@ import "../nav_bar/NavigationBar.css"
 import Star from "../../assets/star.svg"
 import {Link} from "react-router-dom";
 
-const SettingsNavBar = () => {
+const SettingsNavBar = ({ setOpenedTab }) => {
     const [currentPage, setCurrentPage] = useState("General");
-
 
     const handlePageClick = (page) => {
         setCurrentPage(page);
+        setOpenedTab(page);
     };
 
     return (
         <>
             <nav className={`sidebar-settings`}>
-
                 <ul className="nav-items-settings">
-                    <li className={`nav-item ${currentPage === "General" ? "active" : ""}`}>
-                        <Link to={"/settings"}  onClick={() => handlePageClick("General")}>General</Link>
-                        {currentPage === "General" && <span className="arrow"><img src={Star} alt={"star"}/> </span>}
+                    <li
+                        className={`nav-item ${currentPage === "General" ? "active" : ""}`}
+                    >
+                        <Link to={"/settings"} onClick={() => handlePageClick("General")}>
+                            General
+                        </Link>
+                        {currentPage === "General" && (
+                            <span className="arrow">
+                <img src={Star} alt={"star"} />{" "}
+              </span>
+                        )}
                     </li>
-                    <li className={`nav-item ${currentPage === "Password" ? "active" : ""}`}>
-                        <Link  to={"/explore"} onClick={() => handlePageClick("Password")}>Password</Link>
-                        {currentPage === "Password" && <span className="arrow"><img src={Star} alt={"star"}/></span>}
+                    <li
+                        className={`nav-item ${
+                            currentPage === "Password" ? "active" : ""
+                        }`}
+                        onClick={() => handlePageClick("Password")}
+                    >
+                        <Link to={"#"}>Password</Link>
+                        {currentPage === "Password" && (
+                            <span className="arrow">
+                <img src={Star} alt={"star"} />
+              </span>
+                        )}
                     </li>
                     <li className={`nav-item ${currentPage === "Email" ? "active" : ""}`}>
-                        <Link  to={"/explore"} onClick={() => handlePageClick("Email")}>Email</Link>
-                        {currentPage === "Email" && <span className="arrow"><img src={Star} alt={"star"}/></span>}
+                        <Link to={"/explore"} onClick={() => handlePageClick("Email")}>
+                            Email
+                        </Link>
+                        {currentPage === "Email" && (
+                            <span className="arrow">
+                <img src={Star} alt={"star"} />
+              </span>
+                        )}
                     </li>
                 </ul>
             </nav>
         </>
     );
 };
-
 
 export default SettingsNavBar;
