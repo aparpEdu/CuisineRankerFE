e0f9783c909974d4d6547fa938fe752be57ac9ce
moldovexc <alexanderparpulansky@gmail.com>
2024-03-17 00:33:02 +0200

    Removed unused tabs

--- src/components/nav_bar/NavigationBar.jsx
+++ src/components/nav_bar/NavigationBar.jsx
@@ -31,30 +31,31 @@ const NavigationBar = ({currentPage, onChange}) => {
             <nav className={`sidebar ${sidebarOpen ? 'open' : ''}`}>
 
             <ul className="nav-items">
-                <li className={`nav-item ${currentPage === "PROFILE" ? "active" : ""}`}>
-                    <Link to={"/profile"}  onClick={() => handlePageClick("PROFILE")}>PROFILE</Link>
-                    {currentPage === "PROFILE" && <span className="arrow"><img src={Star} alt={"star"}/> </span>}
-                </li>
                 <li className={`nav-item ${currentPage === "EXPLORE" ? "active" : ""}`}>
                     <Link  to={"/explore"} onClick={() => handlePageClick("EXPLORE")}>EXPLORE</Link>
                     {currentPage === "EXPLORE" && <span className="arrow"><img src={Star} alt={"star"}/></span>}
                 </li>
-                <li className={`nav-item ${currentPage === "LEADERBOARDS" ? "active" : ""}`}>
-                    <a href="#" onClick={() => handlePageClick("LEADERBOARDS")}>RANKING</a>
-                    {currentPage === "LEADERBOARDS" && <span className="arrow"><img src={Star} alt={"star"}/></span>}
+                <li className={`nav-item ${currentPage === "PROFILE" ? "active" : ""}`}>
+                    <Link to={"/profile"}  onClick={() => handlePageClick("PROFILE")}>PROFILE</Link>
+                    {currentPage === "PROFILE" && <span className="arrow"><img src={Star} alt={"star"}/> </span>}
                 </li>
-                <li className={`nav-item ${currentPage === "FRIENDS" ? "active" : ""}`}>
-                    <a href="#" onClick={() => handlePageClick("FRIENDS")}>FRIENDS</a>
-                    {currentPage === "FRIENDS" && <span className="arrow"><img src={Star} alt={"star"}/></span>}
-                </li>
-                <li className={`nav-item ${currentPage === "SHOP" ? "active" : ""}`}>
-                    <a href="#" onClick={() => handlePageClick("SHOP")}>SHOP</a>
-                    {currentPage === "SHOP" && <span className="arrow"><img src={Star} alt={"star"}/></span>}
-                </li>
-                <li className={`nav-item ${currentPage === "F.A.Q" ? "active" : ""}`}>
-                    <a href="#" onClick={() => handlePageClick("F.A.Q")}>F.A.Q</a>
-                    {currentPage === "F.A.Q" && <span className="arrow"><img src={Star} alt={"star"}/></span>}
-                </li>
+
+                {/*<li className={`nav-item ${currentPage === "LEADERBOARDS" ? "active" : ""}`}>*/}
+                {/*    <a href="#" onClick={() => handlePageClick("LEADERBOARDS")}>RANKING</a>*/}
+                {/*    {currentPage === "LEADERBOARDS" && <span className="arrow"><img src={Star} alt={"star"}/></span>}*/}
+                {/*</li>*/}
+                {/*<li className={`nav-item ${currentPage === "FRIENDS" ? "active" : ""}`}>*/}
+                {/*    <a href="#" onClick={() => handlePageClick("FRIENDS")}>FRIENDS</a>*/}
+                {/*    {currentPage === "FRIENDS" && <span className="arrow"><img src={Star} alt={"star"}/></span>}*/}
+                {/*</li>*/}
+                {/*<li  className={`nav-item ${currentPage === "SHOP" ? "active" : ""}`}>*/}
+                {/*    <a href="#" onClick={() => handlePageClick("SHOP")}>SHOP</a>*/}
+                {/*    {currentPage === "SHOP" && <span className="arrow"><img src={Star} alt={"star"}/></span>}*/}
+                {/*</li>*/}
+                {/*<li className={`nav-item ${currentPage === "F.A.Q" ? "active" : ""}`}>*/}
+                {/*    <a href="#" onClick={() => handlePageClick("F.A.Q")}>F.A.Q</a>*/}
+                {/*    {currentPage === "F.A.Q" && <span className="arrow"><img src={Star} alt={"star"}/></span>}*/}
+                {/*</li>*/}
                 <li className={`nav-item ${currentPage === "CONTACT US" ? "active" : ""}`}>
                     <Link to={"/contact"} onClick={() => handlePageClick("CONTACT US")}>CONTACT</Link>
                     {currentPage === "CONTACT US" && <span className="arrow"><img src={Star} alt={"star"}/></span>}
