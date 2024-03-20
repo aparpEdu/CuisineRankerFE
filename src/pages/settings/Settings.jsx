7cf9a0c17f1b5194b163bf0932df78a66fc0da7b
moldovexc <alexanderparpulansky@gmail.com>
2024-03-20 16:22:21 +0200

    Setting category for advanced settings

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -4,6 +4,7 @@ import SettingsNavBar from "../../components/settings_nav_bar/SettingsNavBar";
 import GeneralSettings from "../../components/general_settings/GeneralSettings";
 import ChangePassword from "../../components/change_password/ChangePassword";
 import ChangeEmail from "../../components/change_email/ChangeEmail";
+import AdvancedSettings from "../../components/advanced_settings/AdvancedSettings";
 
 
 const Settings = () => {
@@ -18,6 +19,8 @@ const Settings = () => {
                 return <ChangePassword />;
             case "Email":
                 return <ChangeEmail />;
+            case "Advanced":
+                return <AdvancedSettings />;
             default:
                 return null;
         }
