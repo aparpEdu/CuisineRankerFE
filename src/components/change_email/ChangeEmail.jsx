235489a3696977e9005d195cfd2e17c656ed7853
moldovexc <alexanderparpulansky@gmail.com>
2024-04-07 15:48:37 +0300

    Added email confirmation section to email settings category

--- src/components/change_email/ChangeEmail.jsx
+++ src/components/change_email/ChangeEmail.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import api from "../../services/api";
 import Spinner2 from "../spinner/Spinner2";
 
@@ -10,6 +10,11 @@ const ChangeEmail = () =>{
     const [errors, setErrors] = useState({});
     const [success, setSuccess] = useState("");
     const [isLoading, setIsLoading] = useState(false);
+    const [status, setStatus] = useState("");
+    const [statusColor, setStatusColor] = useState("");
+    const [isVerified, setVerified] = useState(false);
+    const [verifySuccess, setVerifySuccess] = useState("");
+    const [email, setEmail] = useState("");
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -40,6 +45,42 @@ const ChangeEmail = () =>{
         }
 
     };
+
+    const handleVerifyEmail =  async () => {
+        try {
+            setIsLoading(true);
+            setVerifySuccess("");
+            await api.post(`/auth/confirm-email/resend?email=${email}`);
+            setIsLoading(false);
+            setVerifySuccess("Email Confirmation was sent to your email!")
+        } catch (e) {
+            console.log(e.response.data.message);
+        }
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await api.get('/users');
+                const verified = response.data.isVerified;
+                const email = response.data.email;
+                setEmail(email);
+                if (verified) {
+                    setStatus("Verified");
+                    setStatusColor("lime");
+                    setVerified(true);
+                } else {
+                    setStatus("Unverified");
+                    setStatusColor("red")
+                    setVerified(false);
+                }
+            } catch (error) {
+                console.error("Error fetching user data:", error);
+            }
+        };
+
+        fetchData();
+    }, []);
 
     const handleOldEmailChange = (e) => {
         setSuccess("");
@@ -74,40 +115,50 @@ const ChangeEmail = () =>{
         return errors;
     };
 
-    return(
-      <div>
-          <form onSubmit={handleSubmit}>
-              <div className="form-group">
-                  <label className={"settings-label"} htmlFor="oldEmail">Current Email:</label>
-                  <input
-                      className={"settings-input"}
-                      type="email"
-                      id="oldEmail"
-                      onChange={handleOldEmailChange}
-                  />
-              </div>
-              {errors.oldEmail && <span className="error">{errors.oldEmail}</span>}
-              <div className="form-group">
-                  <label className={"settings-label"} htmlFor="newEmail">New Email:</label>
-                  <input
-                      className={"settings-input"}
-                      type="email"
-                      id="newEmail"
-                      onChange={handleNewEmailChange}
-                  />
-              </div>
-              {errors.newEmail && <span className="error">{errors.newEmail}</span>}
-              {error && <p className="error" >{error}</p>}
-              {success && <p style={{ color: "yellow" }} className="success-email">{success}</p>}
-              <button type="submit" className="button-save">SAVE CHANGES</button>
-              {isLoading && (
-                  <div className="spinner-overlay">
-                      <Spinner2 />
-                  </div>
-              )}
-          </form>
-      </div>
+    return (
+        <div>
+            <form onSubmit={handleSubmit}>
+                <div className="category-title">
+                    <p>Change Email</p>
+                </div>
+                <div className="form-group">
+                    <label className="settings-label" htmlFor="oldEmail">Current Email:</label>
+                    <input
+                        className="settings-input"
+                        type="email"
+                        id="oldEmail"
+                        onChange={handleOldEmailChange}
+                    />
+                </div>
+                {errors.oldEmail && <span className="error">{errors.oldEmail}</span>}
+                <div className="form-group">
+                    <label className="settings-label" htmlFor="newEmail">New Email:</label>
+                    <input
+                        className="settings-input"
+                        type="email"
+                        id="newEmail"
+                        onChange={handleNewEmailChange}
+                    />
+                </div>
+                {errors.newEmail && <span className="error">{errors.newEmail}</span>}
+                {error && <p className="error">{error}</p>}
+                {success && <p style={{ color: "yellow" }} className="success-email">{success}</p>}
+                <button type="submit" className="button-save">SAVE CHANGES</button>
+                {isLoading && (
+                    <div className="spinner-overlay">
+                        <Spinner2 />
+                    </div>
+                )}
+            </form>
+            <div>
+            <div className="category-title">
+                <p>Email Confirmation</p>
+            </div>
+                <label className="settings-label">STATUS:</label><span style={{color: `${statusColor}`}}>{status}</span>
+                <button className="button-resend" disabled={isVerified} onClick={handleVerifyEmail}>Verify</button>
+            </div>
+            {verifySuccess && <p style={{ color: "yellow" }} className="success-email">{verifySuccess}</p>}
+        </div>
     );
 };
-
 export default ChangeEmail;
