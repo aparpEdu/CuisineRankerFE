20584134b61784ab50fc763d2353ea877e986aa6
moldovexc <alexanderparpulansky@gmail.com>
2024-04-29 22:30:43 +0300

    Renamed add recipe to add recipe button to more appropriately represent its feature

---
+++ src/components/add_recipe/AddRecipeButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import plus from "../../assets/plus.svg"
+
+
+const AddRecipeButton = () => {
+
+
+
+    return (
+        <div className="recipe-square add-recipe-card" style={{ color: '#ffffff' }}>
+            <div className="add-recipe-content" >
+                <img src={plus} alt={"add recipe"} className="add-recipe-icon"/>
+            </div>
+        </div>
+    );
+}
+
+export default AddRecipeButton;
