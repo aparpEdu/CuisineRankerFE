250d23b68e9014db2d7c93433992fb4929485fe3
moldovexc <alexanderparpulansky@gmail.com>
2024-05-03 13:41:00 +0300

    Added filtering and search to personal recipes

--- src/pages/explore/my_recipes/MyRecipes.jsx
+++ src/pages/explore/my_recipes/MyRecipes.jsx
@@ -6,34 +6,47 @@ import AddRecipeButton from "../../../components/add_recipe/AddRecipeButton";
 import AddRecipe from "../../../components/add_recipe/AddRecipe";
 
 const MyRecipes = () => {
-    const [recipes, setRecipes] = useState([]);
     const [showAddRecipeWindow, setShowAddRecipeWindow] = useState(false);
-
+    const [searchTerm, setSearchTerm] = useState('');
+    const [filteredRecipes, setFilteredRecipes] = useState([]);
 
     useEffect(() => {
         const fetchRecipes = async () => {
             try {
                 const response = await api.get('/recipes/personal');
-                setRecipes(response.data);
+                filterRecipes(response.data);
             } catch (error) {
                 console.error('Error fetching recipes:', error);
             }
         };
 
         fetchRecipes();
-    }, [showAddRecipeWindow])
+    }, [showAddRecipeWindow, searchTerm])
 
     const handleDelete = async (id) => {
         const confirmDelete = window.confirm("Are you sure you want to remove this recipe?");
         if (confirmDelete) {
             try {
                 await api.delete(`/recipes/personal/${id}`);
-                setRecipes(recipes.filter(recipe => recipe.id !== id));
+                setFilteredRecipes(filteredRecipes.filter(recipe => recipe.id !== id));
             } catch (error) {
                 console.error('Error deleting recipe:', error);
             }
         }
     }
+
+    const handleSearch = (e) => {
+        const term = e.target.value;
+        setSearchTerm(term);
+    };
+
+
+    const filterRecipes = (data) => {
+        const filtered = data.filter((recipe) =>
+            recipe.name.toLowerCase().includes(searchTerm.toLowerCase())
+        );
+        setFilteredRecipes(filtered);
+    };
 
     const handleCloseAddRecipeWindow = () => {
         setShowAddRecipeWindow(false);
@@ -52,12 +65,20 @@ const MyRecipes = () => {
                 <h2>My Recipes</h2>
                 <div className="title-line"></div>
             </div>
+            <div className={"recipe-control-menu"}>
+                <input
+                    type="text"
+                    placeholder="Search through your recipes"
+                    value={searchTerm}
+                    onChange={handleSearch}
+                />
+            </div>
             {showAddRecipeWindow && (
-                <AddRecipe onClose={handleCloseAddRecipeWindow} />
+                <AddRecipe onClose={handleCloseAddRecipeWindow}/>
             )}
-        <div className="recipe-container">
+            <div className="recipe-container">
             <div onClick={handleOpenAddRecipeWindow}><AddRecipeButton /></div>
-            {recipes.map(recipe => (
+            {filteredRecipes.map(recipe => (
                 <div key={recipe.id} className="recipe-square">
                     <img src={recipe.pictureURL} alt={recipe.name} className="recipe-image" />
                     <div className="recipe-info">
