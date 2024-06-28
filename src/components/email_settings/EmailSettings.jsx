f48976da3ce95a0c0b61b06f900d442b284367be
moldovexc <alexanderparpulansky@gmail.com>
2024-06-28 22:18:44 +0300

    Refactored Email Settings panel to use toast messages for certain messages/erros

--- src/components/email_settings/EmailSettings.jsx
+++ src/components/email_settings/EmailSettings.jsx
@@ -1,19 +1,18 @@
 import React, {useEffect, useState} from "react";
 import api from "../../services/api";
 import Spinner2 from "../spinner/Spinner2";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 
 const EmailSettings = () =>{
     const [oldEmail, setOldEmail] = useState("");
     const [newEmail, setNewEmail] = useState("");
-    const [error, setError] = useState("");
     const [errors, setErrors] = useState({});
-    const [success, setSuccess] = useState("");
     const [isLoading, setIsLoading] = useState(false);
     const [status, setStatus] = useState("");
     const [statusColor, setStatusColor] = useState("");
     const [isVerified, setVerified] = useState(false);
-    const [verifySuccess, setVerifySuccess] = useState("");
     const [email, setEmail] = useState("");
 
     const handleSubmit = async (e) => {
@@ -26,22 +25,16 @@ const EmailSettings = () =>{
         }
         try {
             setIsLoading(true);
-            setError("");
-            setSuccess("");
 
             await api.post("/auth/change-email", {
                 oldEmail: oldEmail,
                 newEmail: newEmail
             });
             setIsLoading(false);
-            setSuccess("Additional confirmation needed. Email was sent to " + oldEmail);
+            toast.info("Additional confirmation needed. Email was sent to " + oldEmail);
         } catch (e) {
             setIsLoading(false);
-            if (e.response.data.message.length > 50) {
-                setError("Internal Server Error");
-            } else {
-                setError(e.response.data.message);
-            }
+            toast.error(e.response.data.message);
         }
 
     };
@@ -49,12 +42,11 @@ const EmailSettings = () =>{
     const handleVerifyEmail =  async () => {
         try {
             setIsLoading(true);
-            setVerifySuccess("");
             await api.post(`/auth/confirm-email/resend?email=${email}`);
             setIsLoading(false);
-            setVerifySuccess("Email Confirmation was sent to your email!")
+            toast.info("Email Confirmation was sent to your email!")
         } catch (e) {
-            console.log(e.response.data.message);
+            toast.error("Email Confirmation could not be sent. Please try again.");
         }
     }
 
@@ -83,12 +75,10 @@ const EmailSettings = () =>{
     }, []);
 
     const handleOldEmailChange = (e) => {
-        setSuccess("");
         setOldEmail(e.target.value);
     };
 
     const handleNewEmailChange = (e) => {
-        setSuccess("");
         setNewEmail(e.target.value);
     };
 
@@ -141,8 +131,6 @@ const EmailSettings = () =>{
                     />
                 </div>
                 {errors.newEmail && <span className="error">{errors.newEmail}</span>}
-                {error && <p className="error">{error}</p>}
-                {success && <p style={{ color: "yellow" }} className="success-email">{success}</p>}
                 <button type="submit" className="button-save">SAVE CHANGES</button>
                 {isLoading && (
                     <div className="spinner-overlay">
@@ -157,7 +145,7 @@ const EmailSettings = () =>{
                 <label className="settings-label">STATUS:</label><span style={{color: `${statusColor}`}}>{status}</span>
                 <button className="button-resend" disabled={isVerified} onClick={handleVerifyEmail}>Verify</button>
             </div>
-            {verifySuccess && <p style={{ color: "yellow" }} className="success-email">{verifySuccess}</p>}
+        <ToastContainer />
         </div>
     );
 };
